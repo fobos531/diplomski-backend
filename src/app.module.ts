da32d0c3483019620974e5746ede5150cf0eec12
Jakov Glavina <jakov.glavina@icloud.com>
2022-04-23 21:53:14 +0200

    feat: add tmdb module & test it out

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,5 +14,6 @@ import { UsersModule } from './users/users.module';
   ],
   controllers: [AppController],
   providers: [AppService],
+  exports: [],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TheMovieDbService } from '@harshppatel/nestjs-themoviedb-api';
+
 import { User } from './schemas/user.schema';
 
 import { UsersService } from './users.service';
@@ -7,11 +9,25 @@ import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private readonly tmdb: TheMovieDbService,
+  ) {}
 
   @Get()
   async findAll(): Promise<User[]> {
     return await this.usersService.findAll();
+  }
+
+  @Get('/movies')
+  async getMovies(): Promise<any> {
+    const args = {
+      pathParameters: {
+        movie_id: 384018,
+      },
+    };
+
+    return await this.tmdb.getMovieEndpoint().getDetails(args);
   }
 
   @Post()
