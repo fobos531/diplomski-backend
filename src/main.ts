31e0c7ad5dc530ec459edcd6a4a7990cc4ee856f
Jakov Glavina <jakov.glavina@icloud.com>
2022-04-25 10:59:58 +0200

    chore: set global prefix "api" for all routes

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
