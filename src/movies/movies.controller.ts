20dfa2679eebf36a0687ffceb65a6e24607cf46f
Jakov Glavina <jakov.glavina@icloud.com>
2022-04-26 21:43:36 +0200

    feat: movies controller - get trending titles by type from path params

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,16 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { TheMovieDbService } from '@harshppatel/nestjs-themoviedb-api';
 import MovieDB from 'node-themoviedb';
 
+// Ovo mozda mores zamijeniti umjesto movies da bude titles
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly tmdb: TheMovieDbService) {}
 
-  @Get('/trending')
-  async getMovies(): Promise<any> {
+  @Get('/trending/:type')
+  async getMovies(@Param() params): Promise<any> {
     const args: MovieDB.Arguments.Trending.GetTrending = {
       pathParameters: {
-        media_type: 'all',
+        media_type: params.type,
         time_window: 'week',
       },
     };
