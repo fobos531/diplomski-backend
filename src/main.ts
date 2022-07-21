bd774f1dcaa3306b2ca48846094731fa2a9d445a
Jakov Glavina <jakov.glavina@icloud.com>
2022-07-21 11:49:47 +0200

    chore: fixes

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
