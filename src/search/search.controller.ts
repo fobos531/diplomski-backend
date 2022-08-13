1eb0d810903150c78e08cdda8bb1b71480994d74
Jakov Glavina <jakov.glavina@icloud.com>
2022-08-13 12:17:50 +0200

    chore: cleanup search controller

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TheMovieDbService } from '@harshppatel/nestjs-themoviedb-api';
 import MovieDB from 'node-themoviedb';
 import { SearchDto } from './dto/search.dto';
@@ -7,63 +7,20 @@ import { SearchDto } from './dto/search.dto';
 export class SearchController {
   constructor(private readonly tmdb: TheMovieDbService) {}
 
-  @Post('/')
+  @Post('/movies')
   async multiSearch(@Body() searchDto: SearchDto) {
-    console.log('SEARCH DTO', searchDto);
-
-    const args: MovieDB.Arguments.Search.Multi = {
+    const args: MovieDB.Arguments.Search.Movies = {
       query: {
         query: searchDto.query,
         page: searchDto.page,
       },
     };
 
-    const search = await this.tmdb.getSearchEndpoint().multi(args);
+    const search = await this.tmdb.getSearchEndpoint().movies(args);
 
     return {
       success: true,
       data: search.data,
-    };
-  }
-
-  @Get('/:id')
-  async getMovieDetails(@Param('id') id: number): Promise<any> {
-    const args: MovieDB.Arguments.Movie.GetDetails = {
-      pathParameters: {
-        movie_id: id,
-      },
-      query: {
-        append_to_response: 'videos',
-      },
-    };
-
-    const movieDetails = await this.tmdb.getMovieEndpoint().getDetails(args);
-    const images = await this.tmdb.getMovieEndpoint().getImages({
-      pathParameters: { movie_id: id },
-      query: {
-        include_image_language: 'en',
-      },
-    });
-
-    return {
-      success: true,
-      data: { ...movieDetails.data, images: images.data },
-    };
-  }
-
-  @Get('/:id/credits')
-  async getMovieCredits(@Param('id') id: number): Promise<any> {
-    const args: MovieDB.Arguments.Movie.GetCredits = {
-      pathParameters: {
-        movie_id: id,
-      },
-    };
-
-    const credits = await this.tmdb.getMovieEndpoint().getCredits(args);
-
-    return {
-      success: true,
-      data: credits.data,
     };
   }
 }
