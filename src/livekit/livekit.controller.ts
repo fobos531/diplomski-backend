a52e34caa50fe75f8a326891b5a0e206ce984d82
Jakov Glavina <jakov.glavina@icloud.com>
2022-08-27 17:00:07 +0200

    chore: update apikey/secret key for digitalocean

--- src/livekit/livekit.controller.ts
+++ src/livekit/livekit.controller.ts
@@ -3,8 +3,16 @@ import { AccessToken } from 'livekit-server-sdk';
 import { randFullName, randBook } from '@ngneat/falso';
 import { TokenDto } from './dto/token.dto';
 
-const apikey = 'API2NKZfbJBg63w';
-const secretKey = 'YY9q5I95HryiXlKPi2Hhi9m6bENkaPON8CQIEtweXAC';
+// linode
+//const apikey = 'API2NKZfbJBg63w';
+
+// digital ocean
+const apikey = 'APIcS9SFAF47gsu';
+
+// linode
+//const secretKey = 'YY9q5I95HryiXlKPi2Hhi9m6bENkaPON8CQIEtweXAC';
+
+const secretKey = 'PRMsHiDs5F2wffJT3eMWk5yEW923xklAMLrl4IBWuGCA';
 
 @Controller('live')
 export class LiveKitController {
